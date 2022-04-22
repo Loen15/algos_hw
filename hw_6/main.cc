55d604ba21105a1a039e4717dfe6671d80077c6f
Loen15 <59060743+Loen15@users.noreply.github.com>
2022-04-22 07:27:27 +0000

    worked hw2

--- hw_6/main.cc
+++ hw_6/main.cc
@@ -24,33 +24,43 @@
 template < typename T, typename Comparator = std::less<T>>
 int Partition (T* arr, int l, int r, Comparator cmp = Comparator()) {
     // выбираем рандомный эл-мент, далее он будет на позиции l в массиве
+    std::cout << "partition " << std::endl;
     if (l == r)
         return l;
     int pivot_pos = rand() % (r - l) + l;
     std::swap(arr[l], arr[pivot_pos]);
     int i = r;
     int j = r;
+    // for (int f = 0; f < 10; f++) {
     while (true) {
+        // std::cout << "partition2 with i; j " << i << "; " << j << std::endl;
+    
         while (i > l && cmp(arr[l], arr[i])) {
             i--;
             j--;
+            std::cout << i << ">" << l;
         }
         while (j > l && cmp(arr[j],arr[l])) {
             j--;
+            std::cout << j << ">" << l;
         }
         if (j <= l) {
+            std::cout << "exit from partition" << std::endl;
             std::swap(arr[l], arr[i]);
             return i;
+        }
         std::swap(arr[i], arr[j]);
         i--;
         j--;
-
-        }
     }
 }
 
 template < typename T, typename Comparator = std::less<T>>
 T kthStat(T* arr, int k, int n, Comparator cmp = Comparator()) {
+    std::cout  << "kthstat arr with size: " << n << std::endl;
+    // for (int i = 0; i < n; i++) {
+    //     std::cout << "; ";
+    // }
     int l = 0;
     int r = n-1;
     // инициализируем нвозможным значением, для захода в цикл
@@ -58,6 +68,11 @@ T kthStat(T* arr, int k, int n, Comparator cmp = Comparator()) {
     
     while (pivot_pos != k) {
         pivot_pos = Partition<T>(arr, l, r, cmp);
+        std::cout << std::endl << "after partition arr: ";
+        for (int i = 0; i < n; i++) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
         if (pivot_pos > k) {
             r = pivot_pos - 1; 
         } else {
@@ -78,6 +93,7 @@ int main()
     for (int i = 0; i < n; i++) {
         std::cin >> array[i];
     }
+    std::cout << "readed" << std::endl;
     // алгоритм
     int* resultat = new int[3];
     resultat[0] = kthStat<int>(array,n * 1 / 10, n);
