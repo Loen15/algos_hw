b3f90e1206db5e72775990395dd1a3521c809a44
Loen15 <59060743+Loen15@users.noreply.github.com>
2022-06-03 12:17:47 +0000

    realization list graph

--- module_3/hw_1/list_graph/list_graph.cc
+++ module_3/hw_1/list_graph/list_graph.cc
@@ -1,24 +1,39 @@
 #include "list_graph.h"
 
+#include <cassert>
+
 ListGraph::ListGraph(int size) : lists_(size) {}
 
 ListGraph::ListGraph(IGraph const& graph) 
                     : lists_(graph.VerticesCount()) {
-
+    for (int i = 0; i < lists_.size(); i++) {
+        lists_[i] = graph.GetNextVertices(i);
+    }
 }
 
 void ListGraph::AddEdge (int from, int to) {
-
+    assert(0 <= from && from < lists_.size());
+    assert(0 <= to && to < lists_.size());
+    lists_[from].push_back(to);
 }
 
 int ListGraph::VerticesCount() const {
-
+    return lists_.size();
 }
 
 std::vector<int> ListGraph::GetNextVertices(int vertex) const {
-
+    assert(0 <= vertex && vertex < lists_.size());
+    return lists_[vertex];
 }
 
 std::vector<int> ListGraph::GetPrevVertices(int vertex) const {
-
+    assert(0 <= vertex && vertex < lists_.size());
+    std::vector<int> prev_vertices;
+    for (int from = 0; from < lists_.size(); from++) {
+        for (int to : lists_[from]) {
+            if (to == vertex)
+                prev_vertices.push_back(from);
+        }
+    }
+    return prev_vertices;
 }
