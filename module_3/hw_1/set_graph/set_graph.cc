87b96831355e82c7f797a1ad4ac82a9576abde86
Loen15 <59060743+Loen15@users.noreply.github.com>
2022-06-03 13:41:05 +0000

    realization set graph

--- module_3/hw_1/set_graph/set_graph.cc
+++ module_3/hw_1/set_graph/set_graph.cc
@@ -1,24 +1,44 @@
 #include "set_graph.h"
 
-SetGraph::SetGraph(int size) : lists_(size) {}
+#include <cassert>
+
+SetGraph::SetGraph(int size) : sets_(size) {}
 
 SetGraph::SetGraph(IGraph const& graph) 
-                    : lists_(graph.VerticesCount()) {
-
+                    : sets_(graph.VerticesCount()) {
+    for (int from = 0; from < graph.VerticesCount(); from++) {
+        for (auto to : graph.GetNextVertices(from)) {
+            sets_[from].insert(to);
+        }
+    }
 }
 
 void SetGraph::AddEdge (int from, int to) {
-
+    assert(0 <= from && from < sets_.size());
+    assert(0 <= to && to < sets_.size());
+    sets_[from].insert(to);
 }
 
 int SetGraph::VerticesCount() const {
-
+    return sets_.size();
 }
 
 std::vector<int> SetGraph::GetNextVertices(int vertex) const {
-
+    assert(0 <= vertex && vertex < sets_.size());
+    std::vector<int> next_vertices;
+    for (auto to : sets_[vertex]) {
+        next_vertices.push_back(to);
+    }
+    return next_vertices;
 }
 
 std::vector<int> SetGraph::GetPrevVertices(int vertex) const {
-
+    assert(0 <= vertex && vertex < sets_.size());
+    std::vector<int> prev_vertices;
+    for (int from = 0; from < sets_.size(); from++) {
+        if (sets_[from].find(vertex) != sets_[from].end()) {
+            prev_vertices.push_back(from);
+        }
+    }
+    return prev_vertices;    
 }
--- module_3/hw_1/set_graph/set_graph.h
+++ module_3/hw_1/set_graph/set_graph.h
@@ -3,6 +3,8 @@
 
 #include "i_graph.h"
 
+#include <unordered_set>
+
 class SetGraph : public IGraph {
 public:
     explicit SetGraph (int size);
@@ -15,7 +17,7 @@ public:
     std::vector<int> GetNextVertices(int vertex) const override;
     std::vector<int> GetPrevVertices(int vertex) const override;
 private:
-    std::vector<std::vector<int>> lists_; 
+    std::vector<std::unordered_set<int>> sets_;
 };
 
 #endif SET_GRAPH_H
